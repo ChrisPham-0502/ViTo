058121e19c1d3601b8625de3d7d009ef33c4dad4
Nguyen Duy Minh <ngduyminh2301@gmail.com>
2024-05-24 14:04:44 +0700

    Create AI.py
    make AI file to called by Backend

---
+++ ModelAI/AI.py
@@ -0,0 +1,14 @@
+import HairStyle
+import Size_recommend
+import Tryons
+
+def VitoClothes(linkFirebaseHuman: str, linkFirebaseCloth: str): 
+    # return linkFirebaseResult: str   
+    pass
+
+def VitoHair():
+    pass
+
+def VitoSize(weight: str, height: str):
+    #return size: str
+    pass
